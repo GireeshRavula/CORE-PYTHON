15b567311e1242bfc299496580ab4828c9e34cd0
GireeshRavula <167273858+GireeshRavula@users.noreply.github.com>
2025-11-20 17:01:07 +0530

    Add files via upload

--- GENERATORS/Sample.py
+++ GENERATORS/Sample.py
@@ -1,1 +1,16 @@
+# def generator():
+#     yield 1
+#     yield 2
+#     yield 3
+# res = generator()
+# print(res)
 
+def generator():
+    yield 1
+    yield 2
+    yield 3
+res = generator()
+print(next(res))
+print(next(res))
+print(next(res))
+print(next(res))
