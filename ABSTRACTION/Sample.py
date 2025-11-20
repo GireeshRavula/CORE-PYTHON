a82ec01a737dad1eeb086388a7a0935c5e248da6
GireeshRavula <167273858+GireeshRavula@users.noreply.github.com>
2025-11-20 17:02:26 +0530

    Add files via upload

--- ABSTRACTION/Sample.py
+++ ABSTRACTION/Sample.py
@@ -1,1 +1,15 @@
-
+from abc import ABC,abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def makeSound(self):
+        pass
+class Dog(Animal):
+    def makeSound(self):
+        print("Bark")
+class Cat(Animal):
+    def makeSound(self):
+        print("Meow")
+d1 = Dog()
+c1 = Cat()
+d1.makeSound()
+c1.makeSound()
